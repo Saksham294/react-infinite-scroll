d01143a8b10f1f7117015c778a0355ca15454455
Saksham <sakshamsingh5001@gmail.com>
2024-05-06 21:26:55 +0530

    Add Card to Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import './Home.css'
+import Card from '../Card/Card'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='home-container'>
+            <div className="search-filters">
+
+            </div>
+            <div className="matching-jobs">
+                <Card/>
+            </div>
         </div>
     )
 }
